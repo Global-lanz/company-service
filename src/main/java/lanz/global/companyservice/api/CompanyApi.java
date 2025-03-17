044e45e968f64434f961e135174177c1f3d11115
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-03-17 21:13:47 +0000

    Fixed port

--- src/main/java/lanz/global/companyservice/api/CompanyApi.java
+++ src/main/java/lanz/global/companyservice/api/CompanyApi.java
@@ -16,7 +16,6 @@ import lanz.global.companyservice.util.converter.ServiceConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
