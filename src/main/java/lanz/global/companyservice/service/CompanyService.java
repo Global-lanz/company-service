41806481a60d58910401771f47759fb43373592c
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-12 13:31:03 +0100

    Removed the currenyc endpoint

--- src/main/java/lanz/global/companyservice/service/CompanyService.java
+++ src/main/java/lanz/global/companyservice/service/CompanyService.java
@@ -3,9 +3,7 @@ package lanz.global.companyservice.service;
 import lanz.global.companyservice.api.request.CreateCompanyRequest;
 import lanz.global.companyservice.exception.BadRequestException;
 import lanz.global.companyservice.model.Company;
-import lanz.global.companyservice.model.Currency;
 import lanz.global.companyservice.repository.CompanyRepository;
-import lanz.global.companyservice.repository.CurrencyRepository;
 import lanz.global.companyservice.util.converter.ServiceConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,28 +17,24 @@ import java.util.UUID;
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
-    private final CurrencyRepository currencyRepository;
     private final ServiceConverter serviceConverter;
 
-    public List<Currency> findAllCurrencies() {
-        return currencyRepository.findAll();
-    }
 
     public Company createCompany(CreateCompanyRequest request) throws BadRequestException {
-        Currency currency = findCurrencyById(request.currencyId());
+//        Currency currency = findCurrencyById(request.currencyId());
 
         Company company = serviceConverter.convert(request);
-        company.setCurrency(currency);
+        company.setCurrencyId(null);
         return companyRepository.save(company);
     }
 
-    public Currency findCurrencyById(UUID currencyId) throws BadRequestException {
-        if (currencyId == null) {
-            return null;
-        }
-
-        return currencyRepository.findById(currencyId).orElseThrow(() -> new BadRequestException("exception.not-found.title", "exception.not-found.message", "Currency"));
-    }
+//    public Currency findCurrencyById(UUID currencyId) throws BadRequestException {
+//        if (currencyId == null) {
+//            return null;
+//        }
+//
+//        return currencyRepository.findById(currencyId).orElseThrow(() -> new BadRequestException("exception.not-found.title", "exception.not-found.message", "Currency"));
+//    }
 
     public Company findCompanyById(UUID companyId) {
         if (companyId == null) {
